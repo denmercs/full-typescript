33a3508c97f81d0974617aabd688fcba674f80a0
denmercs <denmercs@gmail.com>
2020-12-17 04:34:36 -0600

    added tuples notes and interfaces

---
+++ annotations/interfaces.ts
@@ -0,0 +1,41 @@
+interface Reportable {
+  //   name: string;
+  //   year: Date;
+  //   broken: boolean;
+  summary(): string;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const drinks = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `my drink has ${this.sugar} grams of sugar`;
+  },
+};
+
+// const printVehicle = (vehicle: {
+//   name: string;
+//   year: number;
+//   broken: boolean;
+// }): void => {
+//   console.log(`Name: ${vehicle.name}`);
+//   console.log(`Year: ${vehicle.year}`);
+//   console.log(`Broken: ${vehicle.broken}`);
+// };
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drinks);
--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -4,4 +4,15 @@ const drink = {
   sugar: 40,
 };
 
-const pepsi: [string, boolean, number] = ["brown", true, 40];
+// tuples
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["clear", true, 40];
+const tea: Drink = ["brown", false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
